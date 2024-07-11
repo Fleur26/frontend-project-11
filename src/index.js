a298706abb7d4d49c147b00113da0d990003ba17
Fleur26 <lera.hoholeva@yandex.ru>
2024-07-11 18:43:12 +0400

    try ptomise

--- src/index.js
+++ src/index.js
@@ -1,2 +1,25 @@
 //import  './styles.scss';
 //import  'bootstrap';
+
+function checkLink (link){
+    const splitLink = link.split('/');
+    return false;
+}
+
+let promise = new Promise(function(resolve, reject) {
+ const button = document.body.querySelector('.btn');
+ button.addEventListener("click", (e) => {
+    const check = checkLink('l/link');
+    
+    resolve(check);
+ })
+}).then(
+    function(result) { 
+        if(result){
+            const form = document.querySelector('.form-control');
+            console.log(form);
+            form.classList.add('red');
+        }
+     },
+)
+
