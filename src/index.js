bf11f40a2419904aa70556d2aba8d71406b4da33
Fleur26 <lera.hoholeva@yandex.ru>
2024-07-12 19:52:49 +0400

    try add vewlidation email

--- src/index.js
+++ src/index.js
@@ -1,25 +1,18 @@
-//import  './styles.scss';
-//import  'bootstrap';
+import * as yup from 'yup';
 
-function checkLink (link){
-    const splitLink = link.split('/');
-    return false;
-}
+let schema = yup.object().shape({
+email: yup.string().email('Not a proper email'), // pass your error message string
+});
 
-let promise = new Promise(function(resolve, reject) {
- const button = document.body.querySelector('.btn');
- button.addEventListener("click", (e) => {
-    const check = checkLink('l/link');
-    
-    resolve(check);
- })
-}).then(
-    function(result) { 
-        if(result){
-            const form = document.querySelector('.form-control');
-            console.log(form);
-            form.classList.add('red');
-        }
-     },
-)
+// check validity
+schema
+.validate({
+email: 'not.a.valid.email',
+})
+.catch((err) => {
+document.querySelector('#fInput').classList.add('red');
+console.log(err.name); // ValidationError
+console.log(err.errors); // ['Not a proper email']
+});
+
 
