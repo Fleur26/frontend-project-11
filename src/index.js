eec8a4719c8b6c3eaecc07b1a3d1027d19165cb3
Fleur26 <lera.hoholeva@yandex.ru>
2024-07-16 14:50:57 +0400

    update form

--- src/index.js
+++ src/index.js
@@ -1,22 +1,26 @@
 import * as yup from 'yup';
 
 
+const form = document.querySelector('form');
+console.log(form);
+  form.addEventListener('submit', (e) => {
+  e.preventDefault();
+})
 
 let schema = yup.object().shape({
     website: string().url().nullable()
 });
 
-// check validity
-let url = await schema.validate(await fetchUser());
-
-if(!url){
-    document.querySelector('#url-input').classList.add('red');
-    console.log('not right');
-}
-
-const button = document.querySelector('#btn');
 const input = document.querySelector('#url-input');
+
+// check validity
+let url = await schema.validate(input.value).catch(e); //or then
+
+
+
+const button = document.querySelector('button[type="submit"]');
 button.addEventListener('click',(e)=>{
     input.focus();
     input.value = '';
 });
+
