a199880dac7cae05d669cffbeac077aee5decb25
Fleur26 <lera.hoholeva@yandex.ru>
2024-07-17 19:25:31 +0400

    add validation

--- src/index.js
+++ src/index.js
@@ -1,22 +1,24 @@
 import * as yup from 'yup';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 
-const form = document.querySelector('form');
-console.log(form);
+
+const form = document.querySelector('.rss-form ');
   form.addEventListener('submit', (e) => {
   e.preventDefault();
 })
 
-let schema = yup.object().shape({
-    website: string().url().nullable()
-});
+
+let schema = yup.string().url().nullable().min(3);
 
 const input = document.querySelector('#url-input');
-
+input.value = `dfsdffd`;
 // check validity
-let url = await schema.validate(input.value).catch(e); //or then
-
-
+const url = schema.validate(input.value).then(console.log('good')).catch((e => {
+  input.classList.add('is-invalid');
+  console.log(e);
+}));
 
 const button = document.querySelector('button[type="submit"]');
 button.addEventListener('click',(e)=>{
