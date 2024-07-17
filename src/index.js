55f0f9f0fd1a7d80936894c491bd24d7d7a4d73a
Fleur26 <lera.hoholeva@yandex.ru>
2024-07-17 19:37:49 +0400

    update validate

--- src/index.js
+++ src/index.js
@@ -2,27 +2,42 @@ import * as yup from 'yup';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+const state = {
+  form: {
+    state: 'valid',
+    data: {
+      url: '',
+    },
+    errors: [],
+  },
+};
+
+function validation (url) {
+  let schema = yup.string().url().nullable().min(3)
+  .validate(url)
+  .then(state.form.state = 'valid')
+  .catch((e => {
+    const input = document.querySelector('#url-input');
+    state.form.state = 'invalid'
+    state.form.errors = e; 
+    input.classList.add('is-invalid');
+  }));
+  console.log(schema);
+  return schema;
+}
 
 
 const form = document.querySelector('.rss-form ');
   form.addEventListener('submit', (e) => {
   e.preventDefault();
+  const input = document.querySelector('#url-input');
+  state.form.data.url = input.value;
+  validation(state.form.data.url);
+  input.focus();
+  input.value = '';
 })
 
 
-let schema = yup.string().url().nullable().min(3);
 
-const input = document.querySelector('#url-input');
-input.value = `dfsdffd`;
-// check validity
-const url = schema.validate(input.value).then(console.log('good')).catch((e => {
-  input.classList.add('is-invalid');
-  console.log(e);
-}));
 
-const button = document.querySelector('button[type="submit"]');
-button.addEventListener('click',(e)=>{
-    input.focus();
-    input.value = '';
-});
 
