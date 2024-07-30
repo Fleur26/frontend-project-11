931c85afbb298b50415e93f2f5f6fe9b1ae97152
Fleur26 <lera.hoholeva@yandex.ru>
2024-07-30 16:40:17 +0400

    update parsing

--- src/index.js
+++ src/index.js
@@ -88,5 +88,11 @@ const form = document.querySelector('.rss-form ');
 function parsing (url){
 const parser = new DOMParser();
 const doc = parser.parseFromString(url, "text/html");
+fetch(`https://allorigins.hexlet.app/get?url=${encodeURIComponent(url)}`)
+  .then(response => {
+    if (response.ok) return response.json()
+    throw new Error('Network response was not ok.')
+  })
+  .then(data => console.log(data.contents));
 console.log(doc);
 }
